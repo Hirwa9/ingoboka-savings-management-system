fc676396a7356a1347383780598278c6f03875e1
Hirwa9 <hirwawilly9@gmail.com>
2025-01-08 08:45:21 +0200

    Added text option/prop to display on a loading indicator

--- src/components/LoadingIndicator.jsx
+++ src/components/LoadingIndicator.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const LoadingIndicator = ({ className, icon }) => {
+const LoadingIndicator = ({ icon, text, className }) => {
     return (
         <div className={`flex-column flex-center px-3 py-5 text-muted  ${className !== undefined ? className : ''} `}>
             {icon && icon}
-            <span className={`${icon ? 'mt-5' : ''} loader`}></span>
+            {text && text}
+            <span className={`${icon  ? 'mt-5' : text ? 'mt-4' : ''} loader`}></span>
         </div>
     )
 }
