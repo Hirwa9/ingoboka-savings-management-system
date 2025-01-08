6f96ff789d878da07c1499fc53405fcd2ade6051
Hirwa9 <hirwawilly9@gmail.com>
2025-01-08 09:58:05 +0200

    Added loading spinners recoloring property

--- src/components/LoadingIndicator.jsx
+++ src/components/LoadingIndicator.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const LoadingIndicator = ({ icon, text, className }) => {
+const LoadingIndicator = ({ icon, text, className, loaderColor }) => {
     return (
         <div className={`flex-column flex-center px-3 py-5 text-muted  ${className !== undefined ? className : ''} `}>
             {icon && icon}
             {text && text}
-            <span className={`${icon  ? 'mt-5' : text ? 'mt-4' : ''} loader`}></span>
+            <span className={`${icon ? 'mt-5' : text ? 'mt-4' : ''} ${loaderColor ? `text-${loaderColor}` : ''} loader`}></span>
         </div>
     )
 }
