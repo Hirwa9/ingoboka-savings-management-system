691a0dfefcee4106eaf7c3a895c1d8a51b7ee984
Hirwa9 <hirwawilly9@gmail.com>
2025-01-09 09:08:41 +0200

    Enhanced BS styling

--- src/components/common/FetchError.jsx
+++ src/components/common/FetchError.jsx
@@ -1,11 +1,11 @@
+import React from 'react';
 import { ArrowClockwise } from '@phosphor-icons/react';
-import React from 'react'
 
 const FetchError = ({ errorMessage, retryKeyword, refreshFunction, className }) => {
-    retryKeyword = retryKeyword || "Retry"
+    retryKeyword = retryKeyword || "Retry";
 
     return (
-        <div className={`col-sm-8 col-md-6 col-lg-5 col-xl-4 mx-auto p-3 rounded ${className !== undefined ? className : ''} error-message`}>
+        <div className={`col-sm-8 col-md-6 col-lg-5 col-xl-4 mx-auto p-3 ${className !== undefined ? className : ''} error-message`}>
             <img src="/images/fetch_error_image-transparent.png" alt="Error" className="w-4rem h-4rem mx-auto mb-2 opacity-50" />
             <p className="text-center text-muted small">{errorMessage}</p>
             <button className="btn btn-sm btn-outline-secondary d-block mx-auto border border-secondary border-opacity-25"
