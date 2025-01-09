4d3af1e8afbd2c0087ff510bbfc6621606de87b7
Hirwa9 <hirwawilly9@gmail.com>
2025-01-09 19:04:25 +0200

    Created dynamic not found component

---
+++ src/components/common/NotFount.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ArrowClockwise } from '@phosphor-icons/react';
+
+const NotFount = ({ notFoundMessage, icon, refreshKeyword, refreshFunction, className }) => {
+    refreshKeyword = refreshKeyword || "Refresh";
+
+    return (
+        <div className={`col-sm-8 col-md-6 mx-auto p-3 ${className !== undefined ? className : ''} info-message`}>
+            {icon && icon}
+            <p className="text-center text-muted small">{notFoundMessage}</p>
+            {refreshFunction && (
+                <button className="btn btn-sm btn-outline-secondary d-block mx-auto border border-secondary border-opacity-25"
+                    onClick={() => refreshFunction()}
+                >
+                    <ArrowClockwise weight="bold" size={18} className="me-1" /> {refreshKeyword ? refreshKeyword : ''}
+                </button>
+            )}
+        </div>
+    )
+}
+
+export default NotFount;
