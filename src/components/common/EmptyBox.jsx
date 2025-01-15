eee63384b74df5f5df2e2c56ddbcac16a5371b50
Hirwa9 <hirwawilly9@gmail.com>
2025-01-15 23:32:26 +0200

    Created empty content placeholder(illustration)

---
+++ src/components/common/EmptyBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const EmptyBox = ({ notFoundMessage,  refreshKeyword, refreshFunction, className }) => {
+    refreshKeyword = refreshKeyword || "Refresh";
+
+    return (
+        <div className={`position-relative isolate mx-auto p-3 ${className !== undefined ? className : ''} peak-borders-t info-message`}>
+            <img src="/images/empty_box_svg.svg" alt="Error" className="position-absolute inset-0 inx--1 object-fit-contain ptr-none" />
+            <div className="col-sm-8 col-md-6 mx-auto px-3 pb-3" style={{paddingTop: '7rem'}}>
+                <p className="text-muted text-center small">{notFoundMessage}</p>
+                {refreshFunction && (
+                    <button className="btn btn-sm btn-outline-secondary d-block mx-auto border border-secondary border-opacity-25"
+                        onClick={() => refreshFunction()}
+                    >
+                        {refreshKeyword ? refreshKeyword : ''}
+                    </button>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default EmptyBox;
