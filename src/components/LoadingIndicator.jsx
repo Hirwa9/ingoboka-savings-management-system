4d71ddb1a0431f11916f5e8e3ae8d6d8c7ad308d
Hirwa9 <hirwawilly9@gmail.com>
2025-01-19 22:14:24 +0200

    Enhanced loading indicator UI

--- src/components/LoadingIndicator.jsx
+++ src/components/LoadingIndicator.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const LoadingIndicator = ({ icon, text, className, loaderColor }) => {
     return (
-        <div className={`flex-column flex-center px-3 py-5 text-muted  ${className !== undefined ? className : ''} `}>
-            {icon && icon}
+        <div className={`flex-column flex-center px-3 py-5 text-muted ${className !== undefined ? className : ''} `}>
+            <span className='opacity-25'>{icon && icon}</span>
             {text && text}
             <span className={`${icon ? 'mt-5' : text ? 'mt-4' : ''} ${loaderColor ? `text-${loaderColor}` : ''} loader`}></span>
         </div>
