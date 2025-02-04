09dad643f2b94b6bf305d1638426b21f030df7c0
hirwa9 <hirwawilly9@gmail.com>
2025-02-04 12:04:35 +0200

    Create api config files

---
+++ src/api/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export const Axios = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true, // Allows sending cookies and auth headers
+});
