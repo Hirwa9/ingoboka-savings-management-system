3db0619b792923efe7c5a597d9baf2af47d4ce18
hirwa9 <hirwawilly9@gmail.com>
2025-02-09 20:01:31 +0200

    Decrease backdrop darkness

--- src/components/common/rightFixedCard/RightFixedCard.jsx
+++ src/components/common/rightFixedCard/RightFixedCard.jsx
@@ -10,11 +10,11 @@ const RightFixedCard = ({ show, title = "Information", icon, content, onClose, f
     return (
         <>
             {show && (
-                <div className='position-fixed fixed-top inset-0 bg-black2 px-2 pt-5 pb-3 inx-max'>
+                <div className='position-fixed fixed-top inset-0 bg-black3 px-2 pt-5 pb-3 inx-max'>
                     <div className={`position-relative h-100 ${fitWidth ? 'w-fit' : 'col-sm-7 col-md-5 col-lg-4 col-xxl-3'} mx-auto me-md-0 bg-light border-secondary border-opacity-25 rounded-4 ${className} right-fixed-card`} style={{ animation: 'flyInTop .3s 1' }}>
                         {/* Icon */}
                         <div className="position-absolute start-50 w-fit h-fit px-3 py-1 blur-bg-3px border border-light border-opacity-50 rounded-pill text-truncate" style={{ translate: "-50% -120%", animation: 'flyInBottom .5s 1' }}>
-                            {icon} <span className='smaller text-gray-200'>{title}</span>
+                            {icon} <span className='smaller text-gray-100'>{title}</span>
                         </div>
                         <AbsoluteCloseButton text="primaryColor" onClose={() => onClose()} />
                         {/* Content */}
