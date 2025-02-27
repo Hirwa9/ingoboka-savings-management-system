e408fe2b025916aa8bd6eee75fd47d8fabd82dd2
hirwa9 <hirwawilly9@gmail.com>
2025-02-27 12:03:36 +0200

    Perform auth check before reaching the protected route

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -3,8 +3,11 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthProvider";
 
 const ProtectedRoute = ({ children }) => {
-    const { isAuthenticated } = useAuth();
+    const { isAuthenticated, checkAuthOnMount } = useAuth();
 
+    if (!isAuthenticated) {
+        checkAuthOnMount();
+    }
     return isAuthenticated ? children : <Navigate to="/login" replace />;
 };
 
