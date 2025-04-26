996913606661645621c52341e5ec8d5f8477624f
hirwa9 <hirwawilly9@gmail.com>
2025-04-26 11:55:54 +0200

    Add optional list items' additional class names

--- src/components/common/FlexibleList.jsx
+++ src/components/common/FlexibleList.jsx
@@ -1,8 +1,16 @@
-const FlexibleList = ({ list = [], rounded = true, containerBg = 'gray-300', listItemBg = 'light', color = 'gray-700', className = '' }) => {
+const FlexibleList = ({
+    list = [],
+    rounded = true,
+    containerBg = 'gray-300',
+    listItemBg = 'light',
+    color = 'gray-700',
+    className = '',
+    listItemClassName = ''
+}) => {
     return (
         <ul className={`list-unstyled d-flex align-items-start gap-2 flex-wrap w-fit bg-${containerBg} p-2 ${rounded ? 'rounded-3' : null} ${className}`}>
             {list.map((item, index) => (
-                <li key={index} className={`flex-align-center gap-2 ${item?.icon ? 'ps-3 pe-2' : 'px-3'} py-1 bg-${listItemBg} text-${color} ${rounded ? 'rounded-2' : null}`}>
+                <li key={index} className={`flex-align-center gap-2 ${item?.icon ? 'ps-3 pe-2' : 'px-3'} py-1 bg-${listItemBg} text-${color} ${rounded ? 'rounded-2' : null} ${listItemClassName}`}>
                     <span className='text-capitalize'>{item?.title}</span> {item?.icon && item?.icon}
                 </li>
             ))}
