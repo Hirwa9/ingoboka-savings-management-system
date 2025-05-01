757461c921e41fba09d5ec5a9bfe94b2fc4645ec
hirwa9 <hirwawilly9@gmail.com>
2025-05-01 19:09:28 +0200

    Create section description

---
+++ src/components/common/SectionDescription.jsx
@@ -0,0 +1,16 @@
+const SectionDescription = ({
+    imagePath = '',
+    content = '',
+    showImageOnMobile = false
+}) => {
+    return (
+        <div className="d-lg-flex align-items-center">
+            <img src={imagePath} alt="" className={`col-md-5 ${!showImageOnMobile ? 'd-none d-lg-block' : null}`} />
+            <div className='alert mb-4 rounded-0 smaller fw-light'>
+                {content}
+            </div>
+        </div>
+    )
+}
+
+export default SectionDescription;
