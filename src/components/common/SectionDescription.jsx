45008522dfaa2d1c19c609e606c915fd1d007496
hirwa9 <hirwawilly9@gmail.com>
2025-05-01 22:04:56 +0200

    Remove image gap/tag if no path is provided

--- src/components/common/SectionDescription.jsx
+++ src/components/common/SectionDescription.jsx
@@ -5,7 +5,10 @@ const SectionDescription = ({
 }) => {
     return (
         <div className="d-lg-flex align-items-center">
-            <img src={imagePath} alt="" className={`col-md-5 ${!showImageOnMobile ? 'd-none d-lg-block' : null}`} />
+            {imagePath ?
+                <img src={imagePath} alt="" className={`col-md-5 ${showImageOnMobile ? 'd-none d-lg-block' : null}`} />
+                : null
+            }
             <div className='alert mb-4 rounded-0 smaller fw-light'>
                 {content}
             </div>
